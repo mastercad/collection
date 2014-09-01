1401c2a6583c7c8f289929701365a88bfa142c58
andreas kempe <andreas.kempe@byte-artist.de>
2014-09-01 22:11:46 +0200

    create github repo

---
+++ build/CMakeFiles/collection.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/collection.dir/main.o"
+  "collection.pdb"
+  "collection"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/collection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
